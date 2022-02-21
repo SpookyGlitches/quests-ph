8658fc6125300f4a4667c797f60103c1069f9a1b
SpookyGlitches <earljseph@gmail.com>
2022-02-21 12:38:16 +0800

    test2

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<div>Hello World! test</div>
+			<div>Hello World! test2</div>
 		</div>
 	);
 }
