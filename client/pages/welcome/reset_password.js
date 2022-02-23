85cba67098c4384942ae558b66efda005acf40ee
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-02-23 17:42:53 +0800

    Matched layout with Login page.

--- client/pages/welcome/reset_password.js
+++ client/pages/welcome/reset_password.js
@@ -2,45 +2,30 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
+import { Link as MuiLink } from '@mui/material';
+import Link from 'next/link';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-    typography: {
-        h3: {
-            fontWeight: 600,
-            color: "#755CDE",
-            fontSize:38
-        },
-        h2: {
-            fontWeight: 600,
-            fontSize:46
-        }
-    }
-})
+import Carousel from 'react-material-ui-carousel';
 
 export default function ResetPassword() {
-  const handleSubmit = (event) => {
+  
+    const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
     });
   };
 
   return (
-      <ThemeProvider theme={theme}>
+    //   <ThemeProvider theme={theme}>
         <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
-            <Grid
-            item
-            xs={false}
-            sm={4}
+            <Grid item xs={false} sm={4}
             md={6}
             sx={{
                 bgcolor: '#755CDE',
@@ -48,67 +33,102 @@ export default function ResetPassword() {
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
             }}
+            >
+            <Carousel
+            autoPlay
+            infiniteLoop
+            showThumbs={false}
+            sx={{
+                height: "100%",
+                width: "100%",
+                backgroundColor: "#755CDE",
+            }}
+            >
+            <img
+                style={{ width: "auto", height: "auto%" }}
+                alt="Quests"
+                src="/assets/resetpassword/carousel1.png"
             />
+
+            <img
+                style={{ width: "auto", height: "auto" }}
+                alt="Quests"
+                src="/assets/resetpassword/carousel2.png"
+            />
+
+            <img
+                style={{ width: "auto", height: "auto" }}
+                alt="Quests"
+                src="/assets/resetpassword/carousel3.png"
+            />
+            </Carousel>
+            </Grid>
             <Grid item xs={12} sm={8} md={6} elevation={6} square>
 
-            <Box
+                <Box
                 sx={{
-                my: 20,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                height: "100%",
+                width: "100%",
+                gap: 3,
+                padding: {
+                    xs: "4rem",
+                    md: "7rem",
+                },
                 }}
-            >
-            <Grid item>
-                <Typography variant="h2" color="secondary">
-                    Quests
-                </Typography>
-                <Typography variant="h2">
-                    Reset your password
-                </Typography>
-            </Grid>
-            <Box
-                sx={{
-                my: 3,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                }}
-            >
-
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="secondary"
-                    sx={{ mt: 3, mb: 2 }}
                 >
-                    Reset Password
-                </Button>
-
-                    <Grid item alignContent="center">
-                        Not yet registered?
-                    <Link href="#" variant="body2">
-                        {"Create an account."}
-                    </Link>
-                    </Grid>
+                    <Box>
+                    <Typography
+                        variant="h4"
+                        sx={{
+                        textAlign: "left",
+                        fontWeight: "bold",
+                        color: "#755CDE",
+                        }}
+                    >
+                        Quests
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                        textAlign: "left",
+                        fontWeight: "bold",
+                        color: "black",
+                        }}
+                    >
+                        Reset your password.
+                    </Typography>
+                    </Box>
+                    <Stack direction="column" spacing={2}>
+                    <TextField
+                        fullWidth
+                        required
+                        style={{}}
+                        id="filled-required"
+                        label="Email Address"
+                        sx={{}}
+                    />
+                    </Stack>
+                    <Button variant="contained">Reset Password</Button>
+                    <Box
+                        sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        }}
+                    >
+                        {/* https://stackoverflow.com/questions/66226576/using-the-material-ui-link-component-with-the-next-js-link-component */}
+                        <Typography variant="string">
+                        Not yet registered?{" "}
+                        <Link href="/">
+                            <MuiLink sx={{ cursor: "pointer" }}>Create an account</MuiLink>
+                        </Link>
+                        </Typography>
+                    </Box>
                 </Box>
-                </Box>
-            </Box>
             </Grid>
         </Grid>
-      </ThemeProvider>
   );
 }
