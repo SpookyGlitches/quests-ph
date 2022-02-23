0f185ffe4e71155201cc446d2962d44e04f67c62
Monica <monicabee358@gmail.com>
2022-02-23 19:08:04 +0800

    Added component for registration header and form

---
+++ client/components/Registration/Header.js
@@ -0,0 +1,34 @@
+import { Typography, Box } from "@mui/material"
+
+const Header = () => {
+  return (
+    <>
+      <Box>
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "left",
+            fontWeight: "bold",
+            color: "#755CDE",
+            mt: "-1rem",
+          }}
+        >
+          Quests
+        </Typography>
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "left",
+            fontWeight: "bold",
+            color: "black",
+            mb: "-2.5rem",
+          }}
+        >
+          Create an account.
+        </Typography>
+      </Box>
+    </>
+  )
+}
+
+export default Header
---
+++ client/components/Registration/MemberRegistrationOne.js
@@ -0,0 +1,97 @@
+import * as React from "react"
+import {
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Stack,
+  Link as MuiLink,
+} from "@mui/material"
+import Link from "next/link"
+import DatePicker from "@mui/lab/DatePicker"
+import LocalizationProvider from "@mui/lab/LocalizationProvider"
+import AdapterDateFns from "@mui/lab/AdapterDateFns"
+
+const MemberRegistrationOne = () => {
+  const [value, setValue] = React.useState(null)
+  const handleChange = (newValue) => {
+    setValue(newValue)
+  }
+  const color = "#858393"
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Stack direction="column" spacing={2}>
+            <TextField
+              fullWidth
+              required
+              style={{}}
+              id="filled-required"
+              label="Display Name"
+              sx={{ mt: "-2rem" }}
+            />
+            <TextField
+              fullWidth
+              required
+              style={{}}
+              id="filled-required"
+              label="Full Name"
+              sx={{}}
+            />
+            <DatePicker
+              label="Birthday"
+              value={value}
+              onChange={(newValue) => {
+                setValue(newValue)
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  sx={{
+                    svg: { color },
+                    input: { color },
+                    backgroundColor: "white",
+                    borderColor: "white",
+                    mt: "1rem",
+                    ml: "-1em",
+                    width: "100%",
+                  }}
+                />
+              )}
+            />
+          </Stack>
+          <Button variant="contained" sx={{ mt: "1rem", mb: "1rem" }}>
+            Next
+          </Button>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="string">
+              <Link href="/login">
+                <MuiLink sx={{ cursor: "pointer" }}>
+                  Already have an account?
+                </MuiLink>
+              </Link>
+            </Typography>
+          </Box>
+        </Box>
+      </LocalizationProvider>
+    </>
+  )
+}
+
+export default MemberRegistrationOne
