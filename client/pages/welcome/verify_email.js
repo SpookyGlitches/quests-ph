876fa3ad00729a675d653f581ad31c9c688289e9
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-02-24 15:10:52 +0800

    Fixed errors for verify email page.

--- client/pages/welcome/verify_email.js
+++ client/pages/welcome/verify_email.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -9,36 +8,44 @@ import WelcomeCarousel from '../../components/Welcome/WelcomeCarousel.js';
 export default function VerifyEmail() {
 
   return (
-    //   <ThemeProvider theme={theme}>
-        <Grid container component="main" sx={{ height: '100vh' }}>
-            <CssBaseline />
-            <Grid item xs={false} sm={4}
-            md={6}
-            sx={{
-                bgcolor: '#755CDE',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}
-            >
-            <WelcomeCarousel />
-            </Grid>
-            <Grid item xs={12} sm={8} md={6} elevation={6} square>
-
-                <Box
-                sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                height: "100%",
-                width: "100%",
-                gap: 3,
-                padding: {
-                    xs: "4rem",
-                    md: "7rem",
-                },
-                }}
-                >
+        <Grid
+        container
+        sx={{
+          backgroundColor: "background.paper",
+          height: "100vh",
+          width: "100vw",
+        }}
+        >
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          order={{ xs: 3, sm: 2 }}
+          sx={{ height: "100%", backgroundColor: "gray" }}
+        >
+          <WelcomeCarousel />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          order={{ xs: 2, sm: 3 }}
+          sx={{ height: "100%" }}
+        >
+                  <Box
+                  sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  height: "100%",
+                  width: "100%",
+                  gap: 3,
+                  padding: {
+                      xs: "4rem",
+                      md: "7rem",
+                  },
+                  }}
+                  >
                     <Box>
                     <Typography
                         variant="h4"
