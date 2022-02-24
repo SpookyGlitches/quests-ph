0f50897222b70390d116e5f172fb9d9d18bdd178
MNCB <56833381+nicabee@users.noreply.github.com>
2022-02-24 22:21:37 +0800

    Qs 4 as a user i can login (#10)
    * Added carousel for login
    
    * Added sign in button
    
    * made login screen more responsive
    
    * fixed sign up button
    
    * removed unnecessary code
    
    * Added links for forgot password and register
    
    * cleaned code
    
    * added container
    
    * Added flex wrap to box component
    
    * added minHeight
    
    * Changed to Grid from Box for ordering purposes
    
    * make form responsive
    
    * remove package-lock.json and some packages
    
    * Added assets folder for images
    
    * Fixed theme.js
    
    * test
    
    * added react clamp lines
    
    * Delete package-lock.json
    
    * delete login.css.js
    
    Co-authored-by: SpookyGlitches <earljseph@gmail.com>
    Co-authored-by: SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>

--- client/config/theme.js
+++ client/config/theme.js
@@ -1,21 +1,29 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles"
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#755cde",
-			contrastText: "#ffffff",
-		},
-		text: {
-			primary: "#000000",
-		},
-		background: {
-			default: "#f5f5f5",
-		},
-	},
-	shape: {
-		borderRadius: 10,
-	},
-});
+  palette: {
+    primary: {
+      main: "#755cde",
+      contrastText: "#ffffff",
+    },
+    text: {
+      primary: "#000000",
+    },
+    background: {
+      default: "#f5f5f5",
+    },
+  },
+  components: {
+    MuiLink: {
+      defaultProps: {
+        underline: "hover",
+        cursor: "pointer",
+      },
+    },
+    shape: {
+      borderRadius: 10,
+    },
+  },
+})
 
-export default theme;
+export default theme
