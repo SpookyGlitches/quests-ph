ddfa4c0bdff0e7b56a7d474c56cf7812f25de586
Monica <monicabee358@gmail.com>
2022-02-24 14:49:06 +0800

    test

--- client/pages/login.js
+++ client/pages/login.js
@@ -6,9 +6,9 @@ import {
   Box,
   Stack,
   Link as MuiLink,
-} from "@mui/material"
-import Link from "next/link"
-import Carousel from "react-material-ui-carousel"
+} from '@mui/material';
+import Link from 'next/link';
+import Carousel from 'react-material-ui-carousel';
 
 export default function Home() {
   return (
@@ -16,9 +16,9 @@ export default function Home() {
     <Grid
       container
       sx={{
-        backgroundColor: "background.paper",
-        height: "100vh",
-        width: "100vw",
+        backgroundColor: 'background.paper',
+        height: '100vh',
+        width: '100vw',
       }}
     >
       <Grid
@@ -26,32 +26,32 @@ export default function Home() {
         xs={12}
         sm={6}
         order={{ xs: 3, sm: 2 }}
-        sx={{ height: "100%", backgroundColor: "gray" }}
+        sx={{ height: '100%', backgroundColor: 'gray' }}
       >
         <Carousel
           autoPlay
           infiniteLoop
           showThumbs={false}
           sx={{
-            height: "100%",
-            width: "100%",
-            backgroundColor: "#755CDE",
+            height: '100%',
+            width: '100%',
+            backgroundColor: '#755CDE',
           }}
         >
           <img
-            style={{ width: "auto", height: "auto%" }}
+            style={{ width: 'auto', height: 'auto%' }}
             alt="Quests"
             src="/assets/loginregister/carousel1.png"
           />
 
           <img
-            style={{ width: "auto", height: "auto" }}
+            style={{ width: 'auto', height: 'auto' }}
             alt="Quests"
             src="/assets/loginregister/carousel2.png"
           />
 
           <img
-            style={{ width: "auto", height: "auto" }}
+            style={{ width: 'auto', height: 'auto' }}
             alt="Quests"
             src="/assets/loginregister/carousel3.png"
           />
@@ -62,19 +62,19 @@ export default function Home() {
         xs={12}
         sm={6}
         order={{ xs: 2, sm: 3 }}
-        sx={{ height: "100%" }}
+        sx={{ height: '100%' }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-            height: "100%",
-            width: "100%",
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            height: '100%',
+            width: '100%',
             gap: 3,
             padding: {
-              xs: "4rem",
-              md: "7rem",
+              xs: '4rem',
+              md: '7rem',
             },
           }}
         >
@@ -82,9 +82,9 @@ export default function Home() {
             <Typography
               variant="h4"
               sx={{
-                textAlign: "left",
-                fontWeight: "bold",
-                color: "#755CDE",
+                textAlign: 'left',
+                fontWeight: 'bold',
+                color: '#755CDE',
               }}
             >
               Quests
@@ -92,12 +92,12 @@ export default function Home() {
             <Typography
               variant="h5"
               sx={{
-                textAlign: "left",
-                fontWeight: "bold",
-                color: "black",
+                textAlign: 'left',
+                fontWeight: 'bold',
+                color: 'black',
               }}
             >
-              Sign in to your account.
+              Sign in to your account
             </Typography>
           </Box>
           <Stack direction="column" spacing={2}>
@@ -121,27 +121,27 @@ export default function Home() {
           <Button variant="contained">Sign In</Button>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
           >
             {/* https://stackoverflow.com/questions/66226576/using-the-material-ui-link-component-with-the-next-js-link-component */}
             <Typography variant="string">
-              Not yet registered?{" "}
+              Not yet registered?{' '}
               <Link href="/">
-                <MuiLink sx={{ cursor: "pointer" }}>Create an account</MuiLink>
+                <MuiLink sx={{ cursor: 'pointer' }}>Create an account</MuiLink>
               </Link>
             </Typography>
             <Typography variant="string">
-              Forgot password?{" "}
+              Forgot password?{' '}
               <Link href="/">
-                <MuiLink sx={{ cursor: "pointer" }}>Click here</MuiLink>
+                <MuiLink sx={{ cursor: 'pointer' }}>Click here</MuiLink>
               </Link>
             </Typography>
           </Box>
         </Box>
       </Grid>
     </Grid>
-  )
+  );
 }
