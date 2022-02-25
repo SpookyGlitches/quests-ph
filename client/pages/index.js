828821850d25793f735599b2a7bc4bdf10edadbb
SpookyGlitches <earljseph@gmail.com>
2022-02-25 16:37:15 +0800

    apply prettier only on staged files

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import AppLayout from '../components/Layouts/AppLayout';
-    const x = '14423';
-  console.log(x)
+const x = '14423';
+console.log(x);
 export default function Home() {
   return <AppLayout>hello</AppLayout>;
 }
