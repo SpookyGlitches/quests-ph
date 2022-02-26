822030550141116951d975a7a3f3868bfcc88c2d
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-02-27 00:30:09 +0800

    Added Landing Page layout.

---
+++ client/pages/landing/index.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Footer from '../../components/Landing/Footer'
+import Header from '../../components/Landing/Header'
+import Button from '@mui/material/Button';
+
+export default function Index() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Header/>
+      <CssBaseline />
+      <Box sx={{
+        mt: 5,
+        ml: 21,
+        display: 'flex',
+        flexDirection: 'row'
+      }}>
+        <Container component="main" maxWidth="sm">
+          <Typography 
+              variant="h3"
+              sx={{
+                  textAlign: "left",
+                  fontWeight: "bold",
+                  mt: 3,
+                  justifyContent: "flex-center"
+                  }}
+              
+          >
+            We help individuals achieve their goals.
+          </Typography>
+          <Typography 
+              variant="h5"
+              sx={{
+                  textAlign: "left",
+                  mt: 5
+                  }}
+          >
+            Lorem ipsum dolor sit amet, consectetur abdipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+          <Box sx={{
+            mt: 5,
+            mb: 3,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start'
+          }}>
+
+          <Button 
+              variant="contained"
+          >
+              <Link color="inherit" href="/landing/terms_of_service">
+                  Be a Mentor
+              </Link>
+          </Button>
+          <Button 
+              variant="contained"
+              sx = {{ml: 3}}
+          >
+              <Link color="inherit" href="/landing/privacy_policy">
+                  Be a Mentee
+              </Link>
+          </Button>
+          
+          </Box>
+        </Container>
+        <Container component="main"  sx = {{mt: 3}} maxWidth="sm">
+          <img
+                style={{ width: "400px", height: "300px", alignContent: 'center'}}
+                alt="Landing Image"
+                src="/assets/landing/LandingImage.png"
+            />
+        </Container>
+      </Box>
+      <Footer />
+    </Box>
+  );
+}
