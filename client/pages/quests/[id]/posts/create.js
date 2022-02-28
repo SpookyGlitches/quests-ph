029c6a776d3b864c1fcf27aa6ca7a46b02590ee3
SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>
2022-02-28 19:13:14 +0800

    add create post page and component (#14)

---
+++ client/pages/quests/[id]/posts/create.js
@@ -0,0 +1,11 @@
+import CreatePost from "../../../../components/Quest/Post/CreatePost";
+import AppLayout from "../../../../components/Layouts/AppLayout";
+
+export default function Create() {
+	return (
+		<AppLayout>
+			{/* todo pa ang quest layout */}
+			<CreatePost />
+		</AppLayout>
+	);
+}
