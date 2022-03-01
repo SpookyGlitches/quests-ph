0cba55162ac78cc6bcad617654eb913d0d90f3f4
Monica <monicabee358@gmail.com>
2022-03-01 18:12:23 +0800

    edit for theme.js

--- client/config/theme.js
+++ client/config/theme.js
@@ -1,33 +1,28 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#755cde',
-      contrastText: '#ffffff',
+      main: "#755cde",
+      contrastText: "#ffffff",
     },
     text: {
-      primary: '#000000',
+      primary: "#000000",
     },
     background: {
-      default: '#f5f5f5',
+      default: "#f5f5f5",
     },
   },
   components: {
     MuiLink: {
       defaultProps: {
-        underline: 'hover',
-        cursor: 'pointer',
+        underline: "hover",
+        cursor: "pointer",
       },
     },
     MuiTextField: {
       defaultProps: {
-        variant: 'filled',
-      },
-    },
-    MuiDatePicker: {
-      defaultProps: {
-        variant: 'filled',
+        variant: "filled",
       },
     },
   },
@@ -35,26 +30,11 @@ const theme = createTheme({
     borderRadius: 10,
   },
   typography: {
-    h1: {
-      fontWeight: 'bold',
-    },
-    h2: {
-      fontWeight: 'bold',
-    },
-    h3: {
-      fontWeight: 'bold',
-    },
     h4: {
       fontWeight: 700,
     },
-    h5: {
-      fontWeight: 'bold',
-    },
-    h6: {
-      fontWeight: 'bold',
-    },
     button: {
-      textTransform: 'none',
+      textTransform: "none",
     },
   },
 });
