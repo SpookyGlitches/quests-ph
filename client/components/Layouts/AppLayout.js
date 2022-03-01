dd38f5a80b81d65327cc8028aa42aa81df7ae596
SpookyGlitches <earljseph@gmail.com>
2022-03-01 21:03:29 +0800

    reduce container width

--- client/components/Layouts/AppLayout.js
+++ client/components/Layouts/AppLayout.js
@@ -5,30 +5,48 @@ import Sidebar from "../Common/Sidebar";
 const drawerWidth = 240;
 
 const AppLayout = ({ children }) => {
-	const [mobileOpen, setMobileOpen] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-	const handleDrawerToggle = () => {
-		setMobileOpen(!mobileOpen);
-	};
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<CssBaseline />
-			<Navbar
-				drawerWidth={drawerWidth}
-				handleDrawerToggle={handleDrawerToggle}
-			/>
-			<Sidebar
-				drawerWidth={drawerWidth}
-				mobileOpen={mobileOpen}
-				handleDrawerToggle={handleDrawerToggle}
-			/>
-			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-				<Toolbar />
-				{children}
-			</Box>
-		</Box>
-	);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Navbar
+        drawerWidth={drawerWidth}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <Sidebar
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <Box component="main" sx={{ p: 3, flexGrow: 1 }}>
+        <Toolbar />
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: {
+                xs: "100%",
+                md: "85%",
+                lg: "75%",
+              },
+            }}
+          >
+            {children}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default AppLayout;
