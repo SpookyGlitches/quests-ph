b087f940bbdd55acfef4cb415cd00745314eef83
SpookyGlitches <earljseph@gmail.com>
2022-03-03 16:01:47 +0800

    change column size

--- client/components/Layouts/QuestLayout.js
+++ client/components/Layouts/QuestLayout.js
@@ -1,24 +1,32 @@
 import AppLayout from "./AppLayout";
 import { Box, Grid } from "@mui/material";
 import QuestHeader from "../Quest/QuestHeader";
+import Todo from "../Quest/Task/ToDo";
 
 export default function QuestLayout({ children }) {
   return (
     <AppLayout>
-      <Grid container sx={{ paddingTop: "1.2rem" }} spacing={10}>
-        <Grid item xs={12} md={9}>
+      <Grid
+        container
+        sx={{ paddingTop: "1.2rem" }}
+        columnSpacing={8}
+        rowSpacing={4}
+      >
+        <Grid item xs={12} lg={8}>
           <QuestHeader />
           <Box sx={{ marginTop: "2rem" }}>{children}</Box>
         </Grid>
-        <Grid item xs={12} md={3}>
-          {/* todo: <Box
+        <Grid item xs={12} lg={4}>
+          <Box
             sx={{
               backgroundColor: "background.paper",
               borderRadius: 2,
               height: "auto",
-              padding: "1rem",
+              padding: "0.5rem",
             }}
-          ></Box> */}
+          >
+            <Todo />
+          </Box>
         </Grid>
       </Grid>
     </AppLayout>
