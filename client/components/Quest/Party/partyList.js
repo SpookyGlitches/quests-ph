ef68a9e1382300d9241fd9f1fc351ad2f116af02
Monica <monicabee358@gmail.com>
2022-03-08 21:47:59 +0800

    Edited Party Tab

--- client/components/Quest/Party/partyList.js
+++ client/components/Quest/Party/partyList.js
@@ -9,18 +9,22 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Avatar,
+  CardHeader,
+  Button,
 } from "@mui/material";
+import { deepOrange } from "@mui/material/colors";
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
+function createData(name, calories, firstInitial, fat) {
+  return { name, calories, firstInitial, fat };
 }
 
 const rows = [
-  createData("1", "grapejuice", 100),
-  createData("2", "ur mom", 69),
-  createData("3", "arianaventi", 456),
-  createData("4", "catto", 312),
-  createData("5", "jettreviveme", 123),
+  createData("1", "grapejuice", "g", 100),
+  createData("2", "ur mom", "u", 69),
+  createData("3", "arianaventi", "a", 456),
+  createData("4", "catto", "c", 312),
+  createData("5", "jettreviveme", "j", 123),
 ];
 const partyList = () => {
   return (
@@ -31,14 +35,14 @@ const partyList = () => {
           m: 3,
         }}
       >
-        <Typography variant="h4" sx={{ color: "#755cde", marginTop: "-0.5em" }}>
+        <Typography variant="h4" sx={{ color: "#755cde", marginTop: "0em" }}>
           Party
         </Typography>
         <Stack spacing={2}>
           <TableContainer>
             <Table sx={{ minWidth: 100 }} aria-label="simple table">
               <TableHead>
-                <TableRow style={{ paddingTop: "-1em" }}>
+                <TableRow>
                   <TableCell>#</TableCell>
                   <TableCell align="center">User</TableCell>
                   <TableCell align="right">Points</TableCell>
@@ -48,13 +52,30 @@ const partyList = () => {
                 {rows.map((row) => (
                   <TableRow
                     key={row.name}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    sx={{
+                      "&:last-child td, &:last-child th": {
+                        border: 0,
+                      },
+                    }}
                   >
                     <TableCell component="th" scope="row">
                       {row.name}
                     </TableCell>
-                    <TableCell align="center" sx={{ mr: "1em" }}>
-                      {row.calories}
+                    <TableCell align="center" sx={{}} padding="none">
+                      <CardHeader
+                        avatar={
+                          <Avatar
+                            sx={{
+                              bgcolor: deepOrange[500],
+                              mr: -5,
+                              ml: 5,
+                            }}
+                          >
+                            {row.firstInitial}
+                          </Avatar>
+                        }
+                        title={row.calories}
+                      />
                     </TableCell>
                     <TableCell align="right">{row.fat}</TableCell>
                   </TableRow>
@@ -62,6 +83,26 @@ const partyList = () => {
               </TableBody>
             </Table>
           </TableContainer>
+
+          <Box>
+            <Button
+              variant="outlined"
+              color="primary"
+              style={{
+                maxWidth: "80px",
+                minWidth: "80px",
+              }}
+            >
+              Leave
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ float: "right", maxWidth: "80px", minWidth: "80px" }}
+            >
+              Invite
+            </Button>
+          </Box>
         </Stack>
         {/* <Grid
           item
