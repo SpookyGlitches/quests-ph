01292ed086c357f20baeaba6d6fbacd87e94289e
Monica <monicabee358@gmail.com>
2022-03-10 01:05:01 +0800

    Added forms for mentor

--- client/pages/auth/register/mentor.js
+++ client/pages/auth/register/mentor.js
@@ -1,6 +1,7 @@
 import AuthLayout from "../../../components/Layouts/AuthLayout";
 import AuthHeader from "../../../components/Auth/AuthHeader";
 import { useForm } from "react-hook-form";
+import * as React from "react";
 import {
   TextField,
   Stepper,
@@ -12,6 +13,10 @@ import {
   Typography,
   Box,
   Stack,
+  //   InputLabel,
+  MenuItem,
+  //   FormControl,
+  //   Select,
   Link as MuiLink,
 } from "@mui/material";
 import Link from "next/link";
@@ -19,34 +24,35 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import DatePicker from "@mui/lab/DatePicker";
 import { Controller } from "react-hook-form";
-import * as yup from "yup";
+// import * as yup from "yup";
 import moment from "moment";
 import { useState } from "react";
 import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+// import { DropzoneArea } from "material-ui-dropzone";
 
-const stepOneValidations = yup.object().shape({
-  displayName: yup.string().required("Please enter a display name"),
-  dateOfBirth: yup
-    .string()
-    .nullable()
-    .test("dateOfBirth", "You must be 18 years or older", function (value) {
-      return moment().diff(moment(value, "YYYY-MM-DD"), "years") >= 18;
-    })
-    .required("Please enter your age"),
-});
-const stepTwoValidations = yup.object().shape({
-  email: yup.string().required("Email is required").email("Email is invalid"),
-  password: yup
-    .string()
-    .required("Password is required")
-    .min(6, "Password must be at least 6 characters")
-    .max(40, "Password must not exceed 40 characters"),
-  confirmPassword: yup
-    .string()
-    .required("Confirm Password is required")
-    .oneOf([yup.ref("password"), null], "Confirm Password does not match"),
-});
-const steps = ["", ""];
+// const stepOneValidations = yup.object().shape({
+//   displayName: yup.string().required("Please enter a display name"),
+//   dateOfBirth: yup
+//     .string()
+//     .nullable()
+//     .test("dateOfBirth", "You must be 18 years or older", function (value) {
+//       return moment().diff(moment(value, "YYYY-MM-DD"), "years") >= 18;
+//     })
+//     .required("Please enter your age"),
+// });
+// const stepTwoValidations = yup.object().shape({
+//   email: yup.string().required("Email is required").email("Email is invalid"),
+//   password: yup
+//     .string()
+//     .required("Password is required")
+//     .min(6, "Password must be at least 6 characters")
+//     .max(40, "Password must not exceed 40 characters"),
+//   confirmPassword: yup
+//     .string()
+//     .required("Confirm Password is required")
+//     .oneOf([yup.ref("password"), null], "Confirm Password does not match"),
+// });
+const steps = ["", "", ""];
 export default function Register() {
   const [activeStep, setActiveStep] = useState(0);
   const [showPassword, setShowPassword] = useState(false);
@@ -64,17 +70,17 @@ export default function Register() {
   const {
     register,
     handleSubmit,
-    watch,
+    // watch,
     control,
     handleOnChange,
     //eslint-disable-next-line
     formState: { errors },
   } = useForm(stateSchema);
-  const watchdisplayName = watch("displayName", "");
-  const watchDateofBirth = watch("dateOfBirth", "");
-  const watchEmail = watch("email", "");
-  const watchPassword = watch("password", "");
-  const watchConfirmPassword = watch("confirmPassword", "");
+  //   const watchdisplayName = watch("displayName", "");
+  //   const watchDateofBirth = watch("dateOfBirth", "");
+  //   const watchEmail = watch("email", "");
+  //   const watchPassword = watch("password", "");
+  //   const watchConfirmPassword = watch("confirmPassword", "");
 
   const onSubmit = (values) => {
     alert(JSON.stringify(values));
@@ -82,42 +88,52 @@ export default function Register() {
   const handleNext = async () => {
     switch (activeStep) {
       case 0:
-        stepOneValidations
-          .validate({
-            displayName: watchdisplayName,
-            dateOfBirth: watchDateofBirth,
-          })
-          //eslint-disable-next-line
-          .then((value) => {
-            alert("success");
+        // stepOneValidations
+        //   .validate({
+        //     displayName: watchdisplayName,
+        //     dateOfBirth: watchDateofBirth,
+        //   })
+        //   //eslint-disable-next-line
+        //   .then((value) => {
+        //     alert("success");
 
-            setActiveStep((prevActiveStep) => prevActiveStep + 1);
-          })
-          .catch((err) => {
-            alert(err);
-            return;
-          });
+        //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        //   })
+        //   .catch((err) => {
+        //     alert(err);
+        //     return;
+        //   });
+        console.log("case 0");
+        alert("case 0");
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
         break;
       case 1:
-        stepTwoValidations
-          .validate({
-            email: watchEmail,
-            password: watchPassword,
-            confirmPassword: watchConfirmPassword,
-          })
-          //eslint-disable-next-line
-          .then((value) => {
-            alert("success2");
+        // stepTwoValidations
+        //   .validate({
+        //     email: watchEmail,
+        //     password: watchPassword,
+        //     confirmPassword: watchConfirmPassword,
+        //   })
+        //   //eslint-disable-next-line
+        //   .then((value) => {
+        //     alert("success2");
 
-            setActiveStep((prevActiveStep) => prevActiveStep + 1);
-          })
-          .catch((err) => {
-            alert(err);
-            return;
-          });
+        //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        //   })
+        //   .catch((err) => {
+        //     alert(err);
+        //     return;
+        //   });
+        console.log("case 1");
+        alert("case 1");
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
         break;
       case 2:
+        console.log("case 2");
+        alert("case 2");
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        break;
+      case 3:
         handleSubmit(onSubmit)();
       // submit form
     }
@@ -126,6 +142,21 @@ export default function Register() {
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+  const [age, setAge] = React.useState("");
+  //   const handleChange = (event) => {
+  //     setAge(event.target.value);
+  //   };
+
+  const answers = [
+    {
+      value: "1",
+      label: "Yes",
+    },
+    {
+      value: "2",
+      label: "No",
+    },
+  ];
 
   return (
     <AuthLayout>
@@ -142,7 +173,6 @@ export default function Register() {
           );
         })}
       </Stepper>
-
       {activeStep == 0 ? (
         <>
           <Stack direction="column" spacing={2}>
@@ -256,6 +286,9 @@ export default function Register() {
           </Stack>
         </>
       ) : (
+        <></>
+      )}
+      {activeStep == 1 ? (
         <>
           <Stack direction="column" spacing={1.5}>
             <Button
@@ -348,40 +381,113 @@ export default function Register() {
               onClick={handleNext}
               sx={{ mt: 5 }}
             >
+              Next
+            </Button>
+          </Stack>
+        </>
+      ) : (
+        <></>
+      )}
+      {activeStep == 2 ? (
+        <>
+          <Stack direction="column" spacing={1.5}>
+            <Button
+              color="primary"
+              variant="contained"
+              type="submit"
+              onClick={handleBack}
+              sx={{
+                mb: 1,
+                boxShadow: 0,
+                ":hover": {
+                  bgcolor: "white",
+                  color: "white",
+                  boxShadow: 0,
+                },
+              }}
+              style={{
+                maxWidth: "100px",
+                minWidth: "100px",
+                backgroundColor: "transparent",
+                color: "#B0B0B0",
+              }}
+            >
+              <ArrowBackIosRoundedIcon
+                style={{ float: "left", marginLeft: "-1.5em" }}
+              />
+              &nbsp; Back
+            </Button>
+            {/* <Typography
+              style={{
+                fontSize: "13px",
+                color: "#989898",
+                marginTop: "0em",
+              }}
+            >
+              Do you have any experience in mentoring?
+            </Typography> */}
+            {/* <FormControl variant="filled" sx={{ m: 1 }}>
+              <InputLabel id="demo-simple-select-label"></InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Yes</MenuItem>
+                <MenuItem value={20}>No</MenuItem>
+              </Select>
+            </FormControl> */}
+
+            <TextField
+              id="gender-select"
+              select
+              label="Do you have any experience in mentoring?"
+              placeholder="Hello" // <<<<< See here
+              value={age}
+              onChange={(data) => setAge(data.target.value)}
+              margin="normal"
+              variant="filled"
+            >
+              {answers.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="If yes, what kind of mentoring?"
+              multiline={true}
+              rows={4}
+              fullWidth
+              sx={{ mt: 2 }}
+              margin="dense"
+              {...register("experience")}
+
+              // error={errors.email ? true : false}
+            />
+
+            {/* <DropzoneArea
+              acceptedFiles={["image/*", "video/*", "application/*"]}
+              showFileNames
+              dropzoneText="Choose a file or drag it here "
+              showAlerts={false}
+              filesLimit={1}
+            /> */}
+            <Button
+              color="primary"
+              variant="contained"
+              type="submit"
+              onClick={handleNext}
+              sx={{ mt: 5 }}
+            >
               Sign Up
             </Button>
           </Stack>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              variant="string"
-              sx={{ mt: "0rem", mb: "1rem" }}
-              textAlign="center"
-            >
-              By signing up, I accept the{" "}
-              <MuiLink
-                sx={{ cursor: "pointer" }}
-                style={{ textDecoration: "none" }}
-              >
-                {/* eslint-disable-next-line */}
-                <a href="/landing/terms-of-service">Quests Terms of Service</a>
-              </MuiLink>{" "}
-              and acknowledge the{" "}
-              <MuiLink
-                sx={{ cursor: "pointer" }}
-                style={{ textDecoration: "none" }}
-              >
-                <a href="/landing/privacy_policy">Privacy Policy</a>
-              </MuiLink>
-              .
-            </Typography>
-          </Box>
         </>
+      ) : (
+        <></>
       )}
     </AuthLayout>
   );
