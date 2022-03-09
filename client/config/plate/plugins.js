e6ac542b856125f2f42f9b8e1a496940df23130b
SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>
2022-03-09 21:02:47 +0800

    Qs 32 quest page posts tab wiki (#32)
    * add wiki
    
    * prettify
    
    * remove styled-components package
    
    * changed imports from 11s to 2s baby
    
    * add dummy value
    
    * add edit and save
    
    * fix bugs and remove packages

---
+++ client/config/plate/plugins.js
@@ -0,0 +1,90 @@
+import {
+  createBoldPlugin,
+  createItalicPlugin,
+  createStrikethroughPlugin,
+  createUnderlinePlugin,
+  MARK_BOLD,
+  MARK_ITALIC,
+  MARK_STRIKETHROUGH,
+  MARK_UNDERLINE,
+} from "@udecode/plate-basic-marks";
+import {
+  createHeadingPlugin,
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_H3,
+} from "@udecode/plate-heading";
+import { createParagraphPlugin } from "@udecode/plate-paragraph";
+import { createLinkPlugin, ELEMENT_LINK } from "@udecode/plate-link";
+import { createListPlugin, ELEMENT_OL, ELEMENT_UL } from "@udecode/plate-list";
+import {
+  createReactPlugin,
+  createHistoryPlugin,
+  createPlugins,
+} from "@udecode/plate-core";
+import styled from "@emotion/styled";
+import { Link } from "@mui/material";
+
+const UnorderedListElement = styled("ul")(() => ({
+  "& > div": {
+    display: "list-item",
+  },
+}));
+
+const OrderedListElement = styled("ol")(() => ({
+  "& > div": {
+    display: "list-item",
+  },
+}));
+
+const LinkElement = (props) => {
+  const { attributes, children, nodeProps, element } = props;
+  return (
+    <Link {...attributes} href={element.url} {...nodeProps}>
+      {children}
+    </Link>
+  );
+};
+
+// hacking!
+const BoldElement = styled("strong")();
+const ItalicElement = styled("em")();
+const UnderlineElement = styled("u")();
+const StrikethroughElement = styled("s")();
+const H1Element = styled("h2")();
+const H2Element = styled("h3")();
+const H3Element = styled("h4")();
+
+export const components = {
+  [ELEMENT_UL]: UnorderedListElement,
+  [ELEMENT_OL]: OrderedListElement,
+  [MARK_BOLD]: BoldElement,
+  [MARK_ITALIC]: ItalicElement,
+  [MARK_UNDERLINE]: UnderlineElement,
+  [MARK_STRIKETHROUGH]: StrikethroughElement,
+  [ELEMENT_H1]: H1Element,
+  [ELEMENT_H2]: H2Element,
+  [ELEMENT_H3]: H3Element,
+  [ELEMENT_LINK]: LinkElement,
+};
+
+export const plugins = createPlugins(
+  [
+    createReactPlugin(),
+    createHistoryPlugin(),
+
+    createParagraphPlugin(),
+    createHeadingPlugin(),
+
+    createBoldPlugin(),
+    createItalicPlugin(),
+    createUnderlinePlugin(),
+    createStrikethroughPlugin(),
+
+    createLinkPlugin(),
+    createListPlugin(),
+  ],
+  {
+    components,
+  },
+);
---
+++ client/pages/quests/[id]/wiki/index.js
@@ -0,0 +1,11 @@
+import QuestLayout from "../../../../components/Layouts/QuestLayout";
+import Wiki from "../../../../components/Quest/Wiki/Wiki";
+
+const Index = () => {
+  return (
+    <QuestLayout>
+      <Wiki />
+    </QuestLayout>
+  );
+};
+export default Index;
