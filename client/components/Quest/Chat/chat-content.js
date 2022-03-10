4a8d5083cfd277a11905e49e31e5cb5ebbcdeb10
rjjfajardo <53226255+rjjfajardo@users.noreply.github.com>
2022-03-10 17:56:53 +0800

    Qs 38 quest page quest id chat (#36)
    * added header component, added chat main content component,  added send chat component
    
    * remove unused imports
    
    * rounded icons, change chat-content layout, change header color, remove border from sent box
    
    * remove package lock

---
+++ client/components/Quest/Chat/chat-content.js
@@ -0,0 +1,170 @@
+import React from "react";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  Box,
+  Typography,
+  ListItemText,
+  Chip,
+  Grid,
+  Divider,
+} from "@mui/material";
+const ChatContent = () => {
+  return (
+    <Box
+      display="column"
+      height="400px"
+      style={{
+        overflow: "hidden",
+        overflowY: "scroll",
+      }}
+    >
+      <Divider>
+        <Chip label="Unread Messages" />
+      </Divider>
+      <List
+        sx={{
+          bgcolor: "background.paper",
+          overflow: "hidden",
+        }}
+      >
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+
+          <ListItemText
+            primary={
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography sx={{ fontSize: "15px", marginRight: 1 }}>
+                  Monica
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#d4d9d4", fontSize: "14px" }}
+                >
+                  Today at 4:50 PM
+                </Typography>
+              </Grid>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline", fontSize: "15px" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Hi, any idea how can i do x?
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+
+          <ListItemText
+            primary={
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography sx={{ fontSize: "15px", marginRight: 1 }}>
+                  Monica
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#d4d9d4", fontSize: "14px" }}
+                >
+                  Today at 4:50 PM
+                </Typography>
+              </Grid>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline", fontSize: "15px" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Hi, any idea how can i do x?
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+
+          <ListItemText
+            primary={
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography sx={{ fontSize: "15px", marginRight: 1 }}>
+                  Monica
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#d4d9d4", fontSize: "14px" }}
+                >
+                  Today at 4:50 PM
+                </Typography>
+              </Grid>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline", fontSize: "15px" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Hi, any idea how can i do x?
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+
+          <ListItemText
+            primary={
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography sx={{ fontSize: "15px", marginRight: 1 }}>
+                  Monica
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#d4d9d4", fontSize: "14px" }}
+                >
+                  Today at 4:50 PM
+                </Typography>
+              </Grid>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline", fontSize: "15px" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Hi, any idea how can i do x?
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+export default ChatContent;
