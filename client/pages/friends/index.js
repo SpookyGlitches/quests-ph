b9197e008f1432ecf88493b218ef52766ac71931
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-03-10 22:53:33 +0800

    Added filter buttons for Friends Page.

--- client/pages/friends/index.js
+++ client/pages/friends/index.js
@@ -1,12 +1,16 @@
 import AppLayout from "../../components/Layouts/AppLayout";
 import SearchBar from "../../components/Friends/Search.js";
 import ListHolder from "../../components/Friends/FriendListHolder.js";
+import FilterHolder from "../../components/Friends/FilterHolder.js";
 
 const Index = () => {
   return (
     <AppLayout>
       <div>
         <SearchBar />
+
+        <FilterHolder />
+
         <ListHolder requestName="Incoming Requests" />
 
         <ListHolder requestName="Outgoing Requests" />
