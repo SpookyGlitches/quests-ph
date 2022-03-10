7453b36851d01a468f412319aa0601bc5287e8b6
Monica <monicabee358@gmail.com>
2022-03-10 16:00:19 +0800

    renaming components

--- client/pages/quests/[id]/party/index.js
+++ client/pages/quests/[id]/party/index.js
@@ -1,5 +1,5 @@
 import PartyList from "../../../../components/Quest/Party/PartyList";
-import BanList from "../../../../components/Quest/Party/BannedList";
+import BanList from "../../../../components/Quest/Party/BanList";
 import { Box, Grid } from "@mui/material";
 import QuestLayout from "../../../../components/Layouts/QuestLayout";
 
