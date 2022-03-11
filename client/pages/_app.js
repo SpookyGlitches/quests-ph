9c5ccd65300c39be4fe7513732edf984d7023f11
SpookyGlitches <earljseph@gmail.com>
2022-03-11 16:42:37 +0800

    remove duplicate import

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "@fontsource/roboto/500.css";
+
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
@@ -8,6 +8,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "../config/theme";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
