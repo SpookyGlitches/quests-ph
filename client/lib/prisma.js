5f88b35c876049da89115ba4b98a6dd177a96ba5
SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>
2022-03-14 14:06:07 +0800

    add nextauth and prisma (#48)
    * add nextauth and prisma
    
    * add .env in gitignore
    
    * delete env

---
+++ client/lib/prisma.js
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+// eslint-disable-next-line import/no-mutable-exports
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
