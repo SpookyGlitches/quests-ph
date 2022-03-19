afd014b7f7b7168c0d783a4e5b7695ff454e7c8a
MNCB <56833381+nicabee@users.noreply.github.com>
2022-03-19 16:31:06 +0800

    Qs 5 user creation fix v2 (#50)
    * Member Reg Fix
    
    * Member Registration Form using RHF
    
    * Removed formik packages
    
    * Added Disclaimer
    
    * removed prop-types
    
    Co-authored-by: SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>

--- client/pages/auth/register/member.js
+++ client/pages/auth/register/member.js
@@ -1,12 +1,12 @@
 import AuthLayout from "../../../components/Layouts/AuthLayout";
 import AuthHeader from "../../../components/Auth/AuthHeader";
-import RegistrationForm from "../../../components/Registration/RegistrationForm";
+import MemberRegistrationForm from "../../../components/Registration/MemberRegistrationForm";
 
 export default function Register() {
   return (
     <AuthLayout>
       <AuthHeader subtitle="Create an account" />
-      <RegistrationForm />
+      <MemberRegistrationForm />
     </AuthLayout>
   );
 }
