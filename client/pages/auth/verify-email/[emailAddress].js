1674aba7b61e6558f092ca99706cf004d90dc1af
Monica <monicabee358@gmail.com>
2022-03-21 18:17:51 +0800

    User is able to resend verification email

--- client/pages/auth/verify-email/[emailAddress].js
+++ client/pages/auth/verify-email/[emailAddress].js
@@ -1,11 +1,29 @@
-import { Button, Stack, Typography, Link as MuiLink } from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
+import { Button, Stack, Typography, Box, Link as MuiLink } from "@mui/material";
 import { useRouter } from "next/router";
 import AuthLayout from "../../../components/Layouts/AuthLayout";
 import AuthHeader from "../../../components/Auth/AuthHeader";
 
 export default function VerifyEmail() {
+  const [message, setMessage] = useState(null);
   const router = useRouter();
   const { emailAddress } = router.query;
+  const email = router.query.emailAddress;
+  // eslint-disable-next-line
+  const clickMe = (parameter) => (event) => {
+    console.log(parameter);
+    const userInfo = {
+      email: parameter,
+    };
+    fetch("/api/auth/verify-email", {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    });
+    setMessage(
+      "We have sent another verification email to your registered email address.",
+    );
+  };
 
   return (
     <AuthLayout>
@@ -19,9 +37,15 @@ export default function VerifyEmail() {
           that we have sent to you. If you have not received an email or want to
           resend the email, please click the button below.
         </Typography>
-        <Button variant="contained" sx={{}}>
+        <Box sx={{ alignItems: "center" }}>
+          <Typography sx={{ fontSize: "12px", color: "primary.main" }}>
+            {message === null ? "" : message}
+          </Typography>
+        </Box>
+        <Button variant="contained" sx={{}} onClick={clickMe(email)}>
           Resend Verification Email
         </Button>
+
         <Typography>
           <MuiLink
             sx={{ cursor: "pointer" }}
