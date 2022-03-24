f00100d7ffd7e011daf4edde5627796193d1c118
Monica <monicabee358@gmail.com>
2022-03-24 19:42:38 +0800

    Added forgot password feature

---
+++ client/pages/api/auth/resetpassword.js
@@ -0,0 +1,58 @@
+import nodemailer from "nodemailer";
+import { v4 as uuidv4 } from "uuid";
+import prisma from "../../../lib/prisma";
+// eslint-disable-next-line
+export default async function (req, res) {
+  // eslint-disable-next-line
+
+  if (req.method === "POST") {
+    const userInfo = JSON.parse(req.body);
+    const tok = uuidv4();
+    console.log(userInfo.email);
+    const checkEmail = await prisma.user.findUnique({
+      where: {
+        email: userInfo.email,
+      },
+    });
+    if (checkEmail) {
+      console.log("Found!");
+      console.log(checkEmail.userId);
+      const createRecord = await prisma.forgotPassword.create({
+        data: {
+          userId: checkEmail.userId,
+          token: tok,
+        },
+      });
+      if (createRecord) {
+        const transporter = nodemailer.createTransport({
+          port: process.env.MAIL_PORT,
+          host: process.env.MAIL_HOST,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
+          },
+          secure: true,
+        });
+        const mailData = {
+          from: process.env.SMTP_USER,
+          to: userInfo.email,
+          subject: `Reset Password`,
+          html: `<div>
+              This is an automated reply from Quests App University of San Carlos. Please do not reply.
+              You are receiving this email because we received a request to reset the password for your account.
+              To proceed, reset your password through this <a href="http://localhost:3000/auth/reset-password/${createRecord.token}">link</a>.
+              The link will only be available for 5 minutes and can only be used once.
+
+          <div>`,
+        };
+
+        transporter.sendMail(mailData, (err, info) => {
+          if (err) console.log(err);
+          else console.log(info);
+        });
+        res.status(200).send({ message: "Success!" });
+      }
+    }
+    await prisma.$disconnect();
+  }
+}
--- client/pages/auth/reset.js
+++ client/pages/auth/reset.js
@@ -5,23 +5,71 @@ import Link from "next/link";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import AuthLayout from "../../components/Layouts/AuthLayout";
 import AuthHeader from "../../components/Auth/AuthHeader";
+import { resetUserPassword } from "../../validations/ResetPassword";
 
 export default function ResetPassword() {
+  // eslint-disable-next-line no-undef
+  const currentValidationSchema = resetUserPassword[0];
+  const formOptions = { resolver: yupResolver(currentValidationSchema) };
+  const { register, handleSubmit, formState } = useForm(formOptions);
+  const { errors } = formState;
+  const onSubmit = (data) => {
+    // console.log(data.email);
+    // eslint-disable-next-line
+    try {
+      // eslint-disable-next-line
+      const res = fetch("/api/auth/resetpassword", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      // if (res.status === 200) {
+      //   Router.push({
+      //     pathname: "/auth/verify-email/[emailAddress]",
+      //     query: { emailAddress: values.email },
+      //   });
+      // } else if (res.status === 403) {
+      //   setMessage("Display Name is already in use.");
+      //   setShow(true);
+      // } else if (res.status === 409) {
+      //   console.log("email");
+      //   setMessage("Email address is already in use.");
+      //   setShow(true);
+      // } else if (res.status === 400) {
+      //   console.log("both");
+      //   setMessage("Display Name and Email Address are already in use.");
+      //   setShow(true);
+      // }
+    } catch (err) {
+      throw err;
+    }
+  };
   return (
     <AuthLayout>
       <AuthHeader subtitle="Reset your password" />
+
       <Stack direction="column" spacing={2}>
-        <TextField
-          fullWidth
-          required
-          style={{}}
-          id="filled-required"
-          label="Email Address"
-        />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            fullWidth
+            name="email"
+            style={{}}
+            id="filled-required"
+            label="Email Address"
+            error={errors.email && errors.email.message}
+            helperText={errors.email ? errors.email.message : ""}
+            {...register("email")}
+          />
+          <Button fullWidth variant="contained" sx={{ mt: 2 }} type="submit">
+            Reset Password
+          </Button>
+        </form>
       </Stack>
-      <Button variant="contained">Reset Password</Button>
+
       <Box
         sx={{
           display: "flex",
@@ -29,7 +77,6 @@ export default function ResetPassword() {
           alignItems: "center",
         }}
       >
-        {/* https://stackoverflow.com/questions/66226576/using-the-material-ui-link-component-with-the-next-js-link-component */}
         <Typography variant="string" align="center">
           Not yet registered?{" "}
           <Link href="/" passHref>
