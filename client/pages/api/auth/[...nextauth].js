db22d5f52bb48ef70b05b6f3ecf3af668c41b5e1
Monica <monicabee358@gmail.com>
2022-03-27 20:33:15 +0800

    Added max age for jwt

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export default NextAuth({
   secret: "test",
   session: {
     strategy: "jwt",
-    // maxAge: 30 * 24 * 60 * 60,
+    maxAge: 30 * 24 * 60 * 60,
   },
   providers: [
     CredentialsProvider({
