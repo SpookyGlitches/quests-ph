7e5fab89b405acad83ddd99da2ad4cda19b2d924
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-03-31 09:46:01 +0800

    Changed minimum value for validation for editing users.

--- client/validations/UserEdit.js
+++ client/validations/UserEdit.js
@@ -12,7 +12,7 @@ export const EditMemberValidations = object({
       /^[a-zA-Z0-9]+$/,
       "Your display name should not contain spaces or special characters",
     )
-    .min(4, "Display Name must be at least 6 characters")
+    .min(6, "Display Name must be at least 6 characters")
     .max(20)
     .required(requiredMsg),
 
