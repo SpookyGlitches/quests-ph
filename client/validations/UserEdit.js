e540779bcc97f83b9c70aa5a04737f991530b121
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-01 14:39:03 +0800

    Removed password validations for Edit Profile Validator.

--- client/validations/UserEdit.js
+++ client/validations/UserEdit.js
@@ -5,7 +5,6 @@ const requiredMsg = "This field is required";
 
 // eslint-disable-next-line import/prefer-default-export
 export const EditMemberValidations = object({
-  // eslint-disable-line no-param-reassign
   fullName: string().required(requiredMsg),
   displayName: string()
     .matches(
@@ -24,20 +23,4 @@ export const EditMemberValidations = object({
     .required("Please enter your age"),
 
   email: string().required(requiredMsg).email("Email is invalid"),
-
-  //   password: string()
-  //     .min(6, "Password must be at least 6 characters")
-  //     .max(40, "Password must not exceed 40 characters")
-  //     .matches(/[a-z]+/, "Password must contain at least one lowercase character")
-  //     .matches(/[A-Z]+/, "Password must contain at least one uppercase character")
-  //     .matches(
-  //       /[@$!%*#?&]+/,
-  //       "Password must contain at least one special character",
-  //     )
-  //     .matches(/\d+/, "Password must contain at least one number")
-  //     .required(requiredMsg),
-
-  //   confirmPassword: string()
-  //     .required(requiredMsg)
-  //     .oneOf([ref("password"), null], "Confirm Password does not match"),
 });
