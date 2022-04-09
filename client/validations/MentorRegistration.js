d08a59e284c9f9690f3d4703e30a3ebc10dc080d
Monica <monicabee358@gmail.com>
2022-04-09 23:51:15 +0800

    validation set to 6 characters

--- client/validations/MentorRegistration.js
+++ client/validations/MentorRegistration.js
@@ -9,7 +9,7 @@ export const stepOneMentorValidations = object({
       /^[a-zA-Z0-9]+$/,
       "Your display name should not contain spaces or special characters",
     )
-    .min(4, "Display Name must be at least 6 characters")
+    .min(6, "Display Name must be at least 6 characters")
     .max(20)
     .required(requiredMsg),
   fullName: string().required(requiredMsg).max(20),
