1e7cf15513213d29f33e4d2c71d48a5304c32a62
Monica <monicabee358@gmail.com>
2022-04-09 20:42:22 +0800

    fixed conditional statements

--- client/pages/api/profile/[userId]/mentorrequest/index.js
+++ client/pages/api/profile/[userId]/mentorrequest/index.js
@@ -16,6 +16,7 @@ async function createRequest(req, res) {
 
 async function checkAvailQuest(req, res) {
   try {
+    // check if request has been submitted but not yet approved/rejected
     const checkAvail = await prisma.questMentorshipRequest.findMany({
       where: {
         questId: Number(req.query.questMentored),
@@ -24,8 +25,19 @@ async function checkAvailQuest(req, res) {
         deletedAt: null,
       },
     });
-
-    return res.status(200).send(checkAvail);
+    // check if request already has a mentor who accepted the quest
+    const checkHasMentor = await prisma.questMentorshipRequest.findMany({
+      where: {
+        questId: Number(req.query.questMentored),
+        status: "ACTIVE",
+        NOT: {
+          updatedAt: null,
+        },
+      },
+    });
+    return res
+      .status(200)
+      .send({ avail: checkAvail, hasMentor: checkHasMentor });
   } catch (err) {
     return res.status(400).send({ message: "Error" });
   }
