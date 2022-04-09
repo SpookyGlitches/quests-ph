b5d21f1eefa44e1f9a792b659bd7516f47242bd3
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-09 20:54:16 +0800

    Query for finding users the currently logged in user does not have a chatroom with.

--- client/pages/api/chats/new/index.js
+++ client/pages/api/chats/new/index.js
@@ -1,10 +1,22 @@
-// import { prisma } from "next-auth/react";
-import { PrismaClient } from "@prisma/client";
+import prisma from "../../../../lib/prisma";
+import { getSession } from "next-auth/react";
 
-const prisma = new PrismaClient();
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    const { user } = await getSession({ req });
+
+    const usersCanChatWith =
+      await prisma.$queryRaw`SELECT userId, displayName, fullName FROM user WHERE userId != ${user.userId} AND userId NOT IN  
+      (SELECT userId from conversationmember WHERE userId !=${user.userId} AND conversationId IN                      
+        (SELECT conversationId FROM conversationmember where userId = ${user.userId} AND deletedAt IS NULL))`;
+
+    /*
+    Description of each SELECT statement in the raw query (per row):
+    This gets the user credentials of the users that the currently logged in user does not have an existing conversation room with.
+    This selects the userId from the conversation room that is paired with the id of the currently logged in user.
+    This gets the conversationIds of the currently logged in user.
+    */
+
+    res.status(200).json(usersCanChatWith);
   }
 }
