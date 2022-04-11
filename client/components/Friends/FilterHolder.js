2ae87e27c7694c1a9724c7ed42b05d7d4f01ea77
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-11 11:40:02 +0800

    Made filter button toggle based on filter picked. No functionality yet.

--- client/components/Friends/FilterHolder.js
+++ client/components/Friends/FilterHolder.js
@@ -1,15 +1,49 @@
 import { Typography, Stack, Button } from "@mui/material";
+import { useState } from "react";
 
 const FilterHolder = () => {
+  const [toggleOutgoing, setToggleOutgoing] = useState(false);
+  const [toggleIncoming, setToggleIncoming] = useState(false);
+  const [toggleFriends, setToggleFriends] = useState(false);
+
+  const handleClickOutgoing = async () => {
+    setToggleOutgoing((prev) => !prev);
+    console.log(toggleOutgoing);
+  };
+
+  const handleClickIncoming = async () => {
+    setToggleIncoming((prev) => !prev);
+    console.log(toggleIncoming);
+  };
+
+  const handleClickFriends = async () => {
+    setToggleFriends((prev) => !prev);
+    console.log(toggleFriends);
+  };
+
   return (
     <Stack sx={{ margin: "2rem" }} spacing={2} direction="row">
       <Button
         sx={{
           backgroundColor: "background.paper",
           borderRadius: (theme) => theme.shape.borderRadius,
+          ...(toggleOutgoing && {
+            backgroundColor: "primary.main",
+            borderRadius: (theme) => theme.shape.borderRadius,
+          }),
         }}
+        onClick={handleClickOutgoing}
       >
-        <Typography sx={{ variant: "body1", fontWeight: 600, p: 0.75 }}>
+        <Typography
+          sx={{
+            variant: "body1",
+            fontWeight: 600,
+            p: 0.75,
+            ...(toggleOutgoing && {
+              color: "primary.contrastText",
+            }),
+          }}
+        >
           Outgoing
         </Typography>
       </Button>
@@ -18,9 +52,23 @@ const FilterHolder = () => {
           backgroundColor: "background.paper",
           borderRadius: (theme) => theme.shape.borderRadius,
           fontWeight: 600,
+          ...(toggleIncoming && {
+            backgroundColor: "primary.main",
+            borderRadius: (theme) => theme.shape.borderRadius,
+          }),
         }}
+        onClick={handleClickIncoming}
       >
-        <Typography sx={{ variant: "body1", fontWeight: 600, p: 0.75 }}>
+        <Typography
+          sx={{
+            variant: "body1",
+            fontWeight: 600,
+            p: 0.75,
+            ...(toggleIncoming && {
+              color: "primary.contrastText",
+            }),
+          }}
+        >
           Incoming
         </Typography>
       </Button>
@@ -28,9 +76,23 @@ const FilterHolder = () => {
         sx={{
           backgroundColor: "background.paper",
           borderRadius: (theme) => theme.shape.borderRadius,
+          ...(toggleFriends && {
+            backgroundColor: "primary.main",
+            borderRadius: (theme) => theme.shape.borderRadius,
+          }),
         }}
+        onClick={handleClickFriends}
       >
-        <Typography sx={{ variant: "body1", fontWeight: 600, p: 0.75 }}>
+        <Typography
+          sx={{
+            variant: "body1",
+            fontWeight: 600,
+            p: 0.75,
+            ...(toggleFriends && {
+              color: "primary.contrastText",
+            }),
+          }}
+        >
           Friends
         </Typography>
       </Button>
