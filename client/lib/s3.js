91a009a46d4b6fd4351966450cee4a8ec52fa19f
SpookyGlitches <59203530+SpookyGlitches@users.noreply.github.com>
2022-04-18 13:03:32 +0800

    Test deploy (#96)
    * add migration file
    
    * remove preview builds
    
    * update seed
    
    * update env variables

--- client/lib/s3.js
+++ client/lib/s3.js
@@ -3,6 +3,10 @@ import { S3Client } from "@aws-sdk/client-s3";
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({
   region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  },
 });
 // eslint-disable-next-line import/prefer-default-export
 export { s3Client };
