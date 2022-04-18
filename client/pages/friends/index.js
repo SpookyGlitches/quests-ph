3220a81460a6824afcd984bea39396616975ad66
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-19 06:52:52 +0800

    Removed filter component and returned no items found if no data is found for a request.

--- client/pages/friends/index.js
+++ client/pages/friends/index.js
@@ -4,7 +4,6 @@ import { Box, Typography } from "@mui/material";
 import axios from "axios";
 import { useSession, getSession } from "next-auth/react";
 import AppLayout from "../../components/Layouts/AppLayout";
-import FilterHolder from "../../components/Friends/FilterHolder";
 import Friends from "../../components/Friends/Friends";
 import Incoming from "../../components/Friends/Incoming";
 import Outgoing from "../../components/Friends/Outgoing";
@@ -12,6 +11,43 @@ import AccessDenied from "../../components/Error/AccessDenied";
 
 function ListHolder({ items, requestName }) {
   const { data: session } = useSession();
+  if (items.length !== 0) {
+    return (
+      <Box
+        sx={{
+          backgroundColor: "background.paper",
+          padding: "1rem",
+          margin: "2rem",
+          display: "flex",
+          flexDirection: "column",
+          borderRadius: 2,
+        }}
+      >
+        <Typography color="primary" variant="h4">
+          {requestName}
+        </Typography>
+
+        {items.map((item) => {
+          switch (requestName) {
+            case "Incoming Requests":
+              return <Incoming key={item.friendRequestId} item={item} />;
+            case "Outgoing Requests":
+              return <Outgoing key={item.friendRequestId} item={item} />;
+            case "Friends":
+              return (
+                <Friends
+                  key={item.friendshipId}
+                  item={item}
+                  displayName={session.user.displayName}
+                />
+              );
+            default:
+              return <h4>Friends</h4>;
+          }
+        })}
+      </Box>
+    );
+  }
   return (
     <Box
       sx={{
@@ -26,25 +62,11 @@ function ListHolder({ items, requestName }) {
       <Typography color="primary" variant="h4">
         {requestName}
       </Typography>
-      {/* eslint-disable-next-line */}
-      {items.map((item) => {
-        switch (requestName) {
-          case "Incoming Requests":
-            return <Incoming key={item.friendRequestId} item={item} />;
-          case "Outgoing Requests":
-            return <Outgoing key={item.friendRequestId} item={item} />;
-          case "Friends":
-            return (
-              <Friends
-                key={item.friendshipId}
-                item={item}
-                displayName={session.user.displayName}
-              />
-            );
-          default:
-            return <h4>Friends</h4>;
-        }
-      })}
+      <Box sx={{ my: 3 }} align="center">
+        <Typography variant="h5" align="center">
+          No items found.
+        </Typography>
+      </Box>
     </Box>
   );
 }
@@ -111,8 +133,6 @@ const Index = () => {
     return (
       <AppLayout>
         <div>
-          <FilterHolder />
-
           <ListHolder items={incoming} requestName="Incoming Requests" />
 
           <ListHolder items={outgoing} requestName="Outgoing Requests" />
