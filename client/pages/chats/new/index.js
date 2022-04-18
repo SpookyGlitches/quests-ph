bc6ecf48a8e01c855da43791de14680717ad6869
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-18 18:48:24 +0800

    Removed old chat and pusher components and old migration file.

--- client/pages/chats/new/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ChatLayout from "../../../components/Layouts/ChatNewLayout";
-
-const ConversationScreen = () => {
-  return <ChatLayout />;
-};
-
-export default ConversationScreen;
