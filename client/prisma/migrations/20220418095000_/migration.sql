18ff2089fc57db5abaea87bec8be4e5db96fadde
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-21 16:45:37 +0800

    Fixed migration files?

--- client/prisma/migrations/20220418095000_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `conversation` DROP FOREIGN KEY `Conversation_questId_fkey`;
-
--- AlterTable
-ALTER TABLE `conversation` MODIFY `questId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Conversation` ADD CONSTRAINT `Conversation_questId_fkey` FOREIGN KEY (`questId`) REFERENCES `Quest`(`questId`) ON DELETE SET NULL ON UPDATE CASCADE;
