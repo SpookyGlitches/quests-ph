4877869d7c0d56bf615807c8b71747c1ec0168e5
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-04-24 08:20:41 +0800

    Fixed statement when no data is found for chats page.

--- client/pages/chats/index.js
+++ client/pages/chats/index.js
@@ -160,7 +160,7 @@ export default function ChatTalkLayout() {
   } else {
     searchFriendBar = (
       <Typography variant="h5" sx={{ mt: 3, color: "#000000" }} align="center">
-        Oops! Looks like you gotta add friends first. :(
+        Oops. You gotta add friends or join a quest first. :(
       </Typography>
     );
     img = (
