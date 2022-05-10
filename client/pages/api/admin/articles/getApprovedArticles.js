05df38d52b098971990f7bc1021c85d0906e4feb
Jerrell Pardo <jerby.pardo28@gmail.com>
2022-05-10 20:04:03 +0800

    Added additional fields when retrieving data for reports and articles.

--- client/pages/api/admin/articles/getApprovedArticles.js
+++ client/pages/api/admin/articles/getApprovedArticles.js
@@ -7,7 +7,7 @@ export default async function getApprovedArticles(req, res) {
 
   try {
     const approvedArticles =
-      await prisma.$queryRaw`SELECT articleId, displayName, category, link FROM User INNER JOIN Article ON Article.userId = User.userId WHERE approvedAt IS NOT NULL AND Article.deletedAt IS NULL`;
+      await prisma.$queryRaw`SELECT articleId, displayName, category, link, SUBSTRING(approvedAt, 1, 10) AS approvedAt  FROM User INNER JOIN Article ON Article.userId = User.userId WHERE approvedAt IS NOT NULL AND Article.deletedAt IS NULL`;
     return res.status(200).json(approvedArticles);
   } catch (error) {
     console.log(error);
